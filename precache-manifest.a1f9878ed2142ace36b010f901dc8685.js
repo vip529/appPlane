dadc798eaf745c630e24ba2f93ae47c1800b9cd4
vip529 <srvip028@gmail.com>
2020-06-27 13:46:51 +0530

    Updates

---
+++ precache-manifest.a1f9878ed2142ace36b010f901dc8685.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62280f926b2ff6062bc240e9b877662d",
+    "url": "/appPlane/index.html"
+  },
+  {
+    "revision": "147b178636941dfee16b",
+    "url": "/appPlane/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "a04d840804cce0ed8b43",
+    "url": "/appPlane/static/css/main.a2959d41.chunk.css"
+  },
+  {
+    "revision": "147b178636941dfee16b",
+    "url": "/appPlane/static/js/2.b8dd5c0a.chunk.js"
+  },
+  {
+    "revision": "975d1ddda2911edd3269aa02fb9bb92e",
+    "url": "/appPlane/static/js/2.b8dd5c0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a04d840804cce0ed8b43",
+    "url": "/appPlane/static/js/main.f4ac37fb.chunk.js"
+  },
+  {
+    "revision": "336e939520670245c607",
+    "url": "/appPlane/static/js/runtime-main.e5380498.js"
+  }
+]);
